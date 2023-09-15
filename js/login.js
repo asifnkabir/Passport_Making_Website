1eb62073aa96c4d9ace3aae115991bd9d949afb5
Asif Nahyan Kabir <asif-amir@hotmail.com>
2023-09-15 20:03:12 +0600

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,8 +1,11 @@
+
 document.getElementById('submit-button').addEventListener('click',function () {
     const userEmail = document.getElementById('email');
     const userEmailValue = userEmail.value;
+
     const userName = document.getElementById('name');
     const userNameValue = userName.value;
+
     const userPassword = document.getElementById('pass');
     const userPasswordValue = userPassword.value;
     
---
+++ js/productsPasses.js
@@ -0,0 +1,4 @@
+
+document.getElementById('button-chill').addEventListener('click',function() {
+    
+})
